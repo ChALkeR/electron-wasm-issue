ee9b05603dc0999caab0fcf6c55b291bbff8227a
Сковорода Никита Андреевич <chalkerx@gmail.com>
2020-12-14 04:53:45 +0300

    Listen on process.env.PORT

--- pure-server.js
+++ pure-server.js
@@ -7,7 +7,7 @@ const PORT = 8080 // 0
 const wasm = fs.readFileSync('d3wasm.wasm')
 const html = fs.readFileSync('index.html')
 
-http.createServer(async (req, res) => {
+const server = http.createServer(async (req, res) => {
   const { url } = req
   if (!url.endsWith('\.wasm')) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
@@ -25,9 +25,17 @@ http.createServer(async (req, res) => {
     console.log('Response part 2/2 sent, opening devtools is now safe')
   }, DELAY * 1000)
 })
-.listen(PORT, '127.0.0.1', function () {
+
+server.on('listening', function () {
   const { address, port } = this.address()
   const url = `http://${address}:${port}/`
   console.log(`Listening on ${url}`)
   console.log(`Try opening devtools on that page! (within ${DELAY} seconds after opening the page)`)
 })
+
+
+if (process.env.PORT) {
+  server.listen(Number(process.env.PORT))
+} else {
+  server.listen(PORT, '127.0.0.1')
+}
